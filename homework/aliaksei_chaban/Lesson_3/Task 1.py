3a2794073fb8e9986673d98b38690ab363281fe0
Aliaksei Chaban <alex2306952412@gmail.com>
2024-11-20 09:50:37 +0100

    The homework 3 is done. Each file equals one task

---
+++ homework/aliaksei_chaban/Lesson_3/Task 1.py
@@ -0,0 +1,17 @@
+# Даны 2 числа a и b. Получить их сумму, разность и произведение
+
+def sum_of_variables(a, b):
+    return a + b
+
+def difference_of_variables(a, b):
+    return a - b
+
+def product_of_variables(a, b):
+    return a * b
+
+first_value = 7
+second_value = 4
+
+print(sum_of_variables(first_value, second_value))
+print(difference_of_variables(first_value, second_value))
+print((product_of_variables(first_value, second_value)))
---
+++ homework/aliaksei_chaban/Lesson_3/Task 3.py
@@ -0,0 +1,13 @@
+# Даны два числа. Найти среднее арифметическое и среднее геометрическое этих чисел
+
+def arithmetic_mean(a, b):
+    return (a + b)/2
+
+def geometric_mean(a, b):
+    return (a * b)**0.5
+
+first_value = 10
+second_value = 5
+
+print(arithmetic_mean(first_value, second_value))
+print(geometric_mean(first_value, second_value))
