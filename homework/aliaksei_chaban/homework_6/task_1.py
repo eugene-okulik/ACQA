e9ad1c9b00e1e85548137c493875ae84cc746b8c
Aliaksei Chaban <alex2306952412@gmail.com>
2024-11-23 11:45:20 +0100

    Aliaksei C: homework_6 fix linters attempt 2

--- homework/aliaksei_chaban/homework_6/task_1.py
+++ homework/aliaksei_chaban/homework_6/task_1.py
@@ -1,4 +1,4 @@
-text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at,' +
+text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at,'
         ' dignissim vitae libero')
 words = text.split()
 modified_words = []
