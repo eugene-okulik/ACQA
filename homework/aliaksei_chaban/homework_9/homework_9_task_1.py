5e1f010117baf5b8cd3bada2693fcf572e090b12
Aliaksei Chaban <alex2306952412@gmail.com>
2024-11-26 10:12:09 +0100

    Aliaksei C: homework_9 fix linters

--- homework/aliaksei_chaban/homework_9/homework_9_task_1.py
+++ homework/aliaksei_chaban/homework_9/homework_9_task_1.py
@@ -1,10 +1,9 @@
 import datetime
 
-
 time = "Jan 15, 2023 - 12:05:33"
 
 python_date = datetime.datetime.strptime(time, '%b %d, %Y - %X')
-print(f'Полное название месяца - {python_date.strftime('%B')}')
+print(f'Полное название месяца - {python_date.strftime("%B")}')
 
 my_formate_date = python_date.strftime('%d.%m.%Y, %H:%M')
 print(f'Новый формат даты - {my_formate_date}')
