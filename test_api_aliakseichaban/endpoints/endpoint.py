c441b9d631bc86335641fe3b65923286c934c176
Aliaksei Chaban <alex2306952412@gmail.com>
2024-12-23 11:46:49 +0100

    Aliaksei C: API testing project, updates according to comments

--- test_api_aliakseichaban/endpoints/endpoint.py
+++ test_api_aliakseichaban/endpoints/endpoint.py
@@ -11,6 +11,6 @@ class Endpoint:
     def check_response_name_is_correct(self, name):
         assert self.json['name'] == name, "The 'Name' parameter hasn't been updated"
 
-    @allure.step('Check status code is 200')
-    def check_status_code_is_200(self):
-        assert self.response.status_code == 200, "Wrong status code. 200 is expected"
+    @allure.step('Check status code')
+    def check_status_code(self, expected_status_code):
+        assert self.response.status_code == expected_status_code, "Wrong status code"
